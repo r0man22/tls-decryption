0e5daf316cca821247085e5b46dd37413aec24e1
Samcr00t <roman.mammadov22@gmail.com>
2024-12-06 11:52:36 -0500

    basis selection process

--- capture.py
+++ capture.py
@@ -1,7 +1,13 @@
-from scapy.all import sniff
+import pyshark
 
-def packet_capturer(packet):
-	if packet.haslayer('TCP') and packet['TCP'].dport == 443:
-		print(packet.show())
+capture = pyshark.LiveCapture(interface='eth0', display_filter='tcp.port == 443')
 
-sniff(prn=packet_capturer, filter="tcp port 443")
+try:
+    with open("captured.txt", "w") as file:
+        for packet in capture.sniff_continuously():
+            file.write(str(packet) + "\n")
+except KeyboardInterrupt:
+    print("Paket yakalama işlemi durduruldu.")
+except EOFError:
+    print("Akış sona erdi.")
+
