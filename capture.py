12a16b998855609dc66bac5402d8ec41776a514a
Samcr00t <roman.mammadov22@gmail.com>
2024-12-10 15:02:22 -0500

    tls packet capture

--- capture.py
+++ capture.py
@@ -1,13 +1,23 @@
 import pyshark
+import asyncio
 
-capture = pyshark.LiveCapture(interface='eth0', display_filter='tcp.port == 443')
+# Paket yakalamayı başlatıyoruz
+capture = pyshark.LiveCapture(interface='eth0', display_filter = 'tls.record.version == 0x0303 or tls.record.version == 0x0304')
 
+# Paket yakalama işlemini dosyaya yazma
 try:
     with open("captured.txt", "w") as file:
+        loop = asyncio.get_event_loop()
         for packet in capture.sniff_continuously():
             file.write(str(packet) + "\n")
+            
 except KeyboardInterrupt:
     print("Paket yakalama işlemi durduruldu.")
+
 except EOFError:
-    print("Akış sona erdi.")
+    print("EOFError: Akış sona erdi.")
+
+finally:
+    # Asyncio olay döngüsünü düzgün bir şekilde kapatıyoruz
+    loop.close()
 
