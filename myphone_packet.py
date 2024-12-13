942e3dd7564683b930a430d2b81a042a992cf1f5
r0man22 <roman.mammadov22@gmail.com>
2024-12-14 00:46:51 +0400

    trying my phone traffic capturing

--- myphone_packet.py
+++ myphone_packet.py
@@ -1,6 +1,6 @@
 import pyshark
 
-target_ip = "192.168.1.100"
+target_ip = "192.168.1.26"
 
 interface = "Wi-Fi"
 
@@ -14,8 +14,8 @@ try:
               src_ip = packet.ip.src
               dst_ip = packet.ip.dst
 
-              if src_ip == target_ip or dost_ip == target_ip:
-                  print(f"[{packet.sniff_time}] {src_ip} -> {dst_ip}")
+              if src_ip == target_ip or dst_ip == target_ip:
+                  print(f"[{packet.sniff_time}] Packet details:\n{packet}")
 
 except KeyboardInterrupt:
     print("\nPacket capturing stopped")
